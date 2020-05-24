7505b712d58706674070bc3e185f2618975ba0ac
Pratama Sumirat <akun.pratamasumirat@gmail.com>
2020-05-24 22:23:31 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import cat from './clement-falize-b9K_LTz079c-unsplash.jpg'
 
 function App() {
+  const [file, setFile] = useState('')
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form">
+        <input className="input" type="text"
+          placeholder="Masukan link Ijazah di sini"
+          name="url" value={file} onChange={e => setFile(e.target.value)}></input>
+      </form>
+      <p>{file ? file : 'Photo by Clément Falize on Unsplash'}</p>
+      <img src={file ? file : cat} alt="PDF tuh. Tunggu lagi loading. Kalau lama coba cut trus paste lagi ya" width="1080"></img>
+      {file ?
+        <iframe title="pdf viewer"
+          src={`https://docs.google.com/viewer?url=${file}&embedded=true`}>
+        </iframe>
+        : null}
     </div>
   );
 }
